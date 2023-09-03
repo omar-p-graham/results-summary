a59fc17666333292b22227cd6b99d44a2067790b
omar-p-graham <142844317+omar-p-graham@users.noreply.github.com>
2023-09-02 20:07:04 -0500

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -3,6 +3,7 @@ fetch('../data.json')
     .then(data => getData(data));
 
 getData = (data)=>{
+    data = JSON.parse(data);
     for(key in data){
         document.querySelector('.'+data[key]['category'].toLowerCase()+' img').setAttribute('src',data[key]['icon']); // insert icons from json file
         document.querySelector('.'+data[key]['category'].toLowerCase()+' img').setAttribute('alt','icon-'+data[key]['category'].toLowerCase()); // insert icons ALT text using json file
